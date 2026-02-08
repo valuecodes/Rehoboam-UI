7db0bcba4a04ea61e15ff97b02208dc82612f915
Juha Kangas <42040080+valuecodes@users.noreply.github.com>
2026-02-08 09:02:34 +0200

    chore: update packages

--- src/components/mainsection/navigation/navbar.tsx
+++ src/components/mainsection/navigation/navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Spring } from "react-spring/renderprops";
+import { animated, useSpring } from "react-spring";
 
 type NavBarProps = {
   initial: boolean;
@@ -16,57 +16,54 @@ export const NavBar = ({
   create,
   cancel,
 }: NavBarProps): React.JSX.Element => {
+  const springProps = useSpring({
+    from: {
+      marginTop: inProgress && !initial ? 0 : -190,
+      cancelMargin: inProgress && !initial ? -190 : -190,
+    },
+    to: {
+      marginTop: inProgress ? -190 : 0,
+      cancelMargin: inProgress && !initial ? 180 : -190,
+    },
+    config: { mass: 3, tension: 600, friction: 100 },
+  });
+
   return (
     <div>
-      <Spring
-        from={{
-          marginTop: inProgress && !initial ? 0 : -190,
-          cancel: inProgress && !initial ? -190 : -190,
+      <animated.div
+        className="navBar"
+        style={{
+          marginTop: springProps.marginTop,
         }}
-        to={{
-          marginTop: inProgress ? -190 : 0,
-          cancel: inProgress && !initial ? 180 : -190,
-        }}
-        config={{ mass: 3, tension: 600, friction: 100 }}
-        key={String(inProgress)}
       >
-        {(springProps: { marginTop: number; cancel: number }) => (
-          <div
-            className="navBar"
-            style={{
-              marginTop: springProps.marginTop,
-            }}
-          >
-            <svg className="navSvg" height="150" width="100%">
-              <polyline
-                points="
-                                5 25,
-                                5 10,
-                                20 0,
-                                220 0,
-                                320 0,
-                                "
-                style={{ fill: "none", stroke: "black", strokeWidth: 1 }}
-              />
-            </svg>
-            <button className="navButton" onClick={restart}>
-              Covid 19
-            </button>
-            <button className="navButton" onClick={create}>
-              Create
-            </button>
-            <button
-              style={{
-                marginTop: springProps.cancel,
-              }}
-              className="navButton cancel"
-              onClick={cancel}
-            >
-              Cancel
-            </button>
-          </div>
-        )}
-      </Spring>
+        <svg className="navSvg" height="150" width="100%">
+          <polyline
+            points="
+                            5 25,
+                            5 10,
+                            20 0,
+                            220 0,
+                            320 0,
+                            "
+            style={{ fill: "none", stroke: "black", strokeWidth: 1 }}
+          />
+        </svg>
+        <button className="navButton" onClick={restart}>
+          Covid 19
+        </button>
+        <button className="navButton" onClick={create}>
+          Create
+        </button>
+        <animated.button
+          style={{
+            marginTop: springProps.cancelMargin,
+          }}
+          className="navButton cancel"
+          onClick={cancel}
+        >
+          Cancel
+        </animated.button>
+      </animated.div>
     </div>
   );
 };
