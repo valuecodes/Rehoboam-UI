3e92dc8eff75a8bd872adb9ee0c8e2ff22d9f26b
Juha Kangas <42040080+valuecodes@users.noreply.github.com>
2026-02-16 22:35:40 +0200

    feat: add api app base

---
+++ apps/api/src/index.ts
@@ -0,0 +1,27 @@
+import { fromHono } from "chanfana";
+import { Hono } from "hono";
+
+import { TaskCreate } from "./endpoints/taskCreate";
+import { TaskDelete } from "./endpoints/taskDelete";
+import { TaskFetch } from "./endpoints/taskFetch";
+import { TaskList } from "./endpoints/taskList";
+
+// Start a Hono app
+const app = new Hono<{ Bindings: Env }>();
+
+// Setup OpenAPI registry
+const openapi = fromHono(app, {
+  docs_url: "/",
+});
+
+// Register OpenAPI endpoints
+openapi.get("/api/tasks", TaskList);
+openapi.post("/api/tasks", TaskCreate);
+openapi.get("/api/tasks/:taskSlug", TaskFetch);
+openapi.delete("/api/tasks/:taskSlug", TaskDelete);
+
+// You may also register routes for non OpenAPI directly on Hono
+// app.get('/test', (c) => c.text('Hono!'))
+
+// Export the Hono app
+export default app;
---
+++ apps/api/src/types.ts
@@ -0,0 +1,13 @@
+import { DateTime, Str } from "chanfana";
+import type { Context } from "hono";
+import { z } from "zod";
+
+export type AppContext = Context<{ Bindings: Env }>;
+
+export const Task = z.object({
+  name: Str({ example: "lorem" }),
+  slug: Str(),
+  description: Str({ required: false }),
+  completed: z.boolean().default(false),
+  due_date: DateTime(),
+});
