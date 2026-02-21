31527db3c45f7025bd53c287cc60dc28898a31bf
Juha Kangas <42040080+valuecodes@users.noreply.github.com>
2026-02-21 15:47:58 +0200

    refactor: replace google fonts with @fontsource

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import "@fontsource/barlow-condensed/300.css";
+import "@fontsource/barlow-condensed/400.css";
+import "@fontsource/barlow-condensed/500.css";
+import "@fontsource/barlow-condensed/600.css";
 import "./index.css";
 
 import { App } from "./app";
